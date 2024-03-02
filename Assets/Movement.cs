84c6853f5b860fda19a1065e4fa407bf285236b4
doximanman <kalo39570@gmail.com>
2024-03-02 21:32:39 +0200

    Better Jumping
    Can only jump when on the ground and character model follows the movement direction (flips accordingly).

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -6,38 +6,52 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public Rigidbody2D playerBody;
+    public SpriteRenderer playerSprite;
     public float HorizontalVelocity = 10;
     public float VerticalVelocity = 10;
 
-    private float RoundToZero=0.00001f;
+    public bool allowJump = true;
+
+    private float RoundToZero = 0.00001f;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            playerBody.velocity=playerBody.velocity.y*Vector2.up+ HorizontalVelocity * Vector2.right;
+            playerBody.velocity = playerBody.velocity.y * Vector2.up + HorizontalVelocity * Vector2.right;
+            playerSprite.flipX = false;
         }
-        else if(Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow)){
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
             playerBody.velocity = playerBody.velocity.y * Vector2.up + HorizontalVelocity * Vector2.left;
+            playerSprite.flipX = true;
         }
         else
         {
-            playerBody.velocity=playerBody.velocity.y*Vector2.up;
+            playerBody.velocity = playerBody.velocity.y * Vector2.up;
         }
 
-        if (Input.GetKey(KeyCode.Space))
+        if (allowJump && Input.GetKey(KeyCode.Space))
         {
-            if (playerBody.velocity.y <= RoundToZero)
-            {
-                playerBody.velocity += VerticalVelocity*Vector2.up;
-            }
+            playerBody.velocity += VerticalVelocity * Vector2.up;
+            allowJump = false;
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            allowJump = true;
+        }
+    }
+
 }
