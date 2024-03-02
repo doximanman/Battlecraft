a2b34e999aedddd2a8369d4e68e1eee6ef03f6be
doximanman <kalo39570@gmail.com>
2024-03-02 21:16:51 +0200

    Jumping

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Movement : MonoBehaviour
 {
     public Rigidbody2D playerBody;
-    public float velocity = 10;
+    public float HorizontalVelocity = 10;
+    public float VerticalVelocity = 10;
+
+    private float RoundToZero=0.00001f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,14 +22,22 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.RightArrow))
         {
-            playerBody.velocity=playerBody.velocity.y*Vector2.up+velocity*Vector2.right;
+            playerBody.velocity=playerBody.velocity.y*Vector2.up+ HorizontalVelocity * Vector2.right;
         }
         else if(Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow)){
-            playerBody.velocity = playerBody.velocity.y * Vector2.up + velocity * Vector2.left;
+            playerBody.velocity = playerBody.velocity.y * Vector2.up + HorizontalVelocity * Vector2.left;
         }
         else
         {
             playerBody.velocity=playerBody.velocity.y*Vector2.up;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (playerBody.velocity.y <= RoundToZero)
+            {
+                playerBody.velocity += VerticalVelocity*Vector2.up;
+            }
         }
     }
 }
