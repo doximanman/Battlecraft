e1d0a8173635df8ce12cdbd0ec43faff362eae3c
doximanman <kalo39570@gmail.com>
2024-03-08 15:23:20 +0200

    Sun and Obsticles

--- Assets/Scripts/Logic.cs
+++ Assets/Scripts/Logic.cs
@@ -7,17 +7,27 @@ public class Logic : MonoBehaviour
     private float maxX;
     private float maxY;
 
+    private List<string> canJumpFrom=new List<string>();
+
     // Start is called before the first frame update
     void Start()
     {
         maxX = 135;
         maxY = 3.5f;
+
+        canJumpFrom.Add("Ground");
+        canJumpFrom.Add("Obsticles");
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public bool canJumpOn(string tag)
+    {
+        return canJumpFrom.Contains(tag);
     }
 
     public float GetMaxX()
