2b0281da93cb16a5bb67f71b22bff1faab34df14
doximanman <kalo39570@gmail.com>
2024-03-12 20:46:13 +0200

    Basic Pause

--- Assets/Scripts/MetaLogic.cs
+++ Assets/Scripts/MetaLogic.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class MetaLogic : MonoBehaviour
 {
@@ -9,12 +10,14 @@ public class MetaLogic : MonoBehaviour
     public static void Pause()
     {
         Time.timeScale = 0;
+        Camera.main.GetComponent<PostProcessVolume>().enabled = true;
         paused = true;
     }
 
     public static void Unpause()
     {
         Time.timeScale = 1;
+        Camera.main.GetComponent<PostProcessVolume>().enabled = false;
         paused = false;
     }
 }
