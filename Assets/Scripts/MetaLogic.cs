39e26cc98b9de8e4912d52141cf80f8c17610398
doximanman <kalo39570@gmail.com>
2024-03-13 15:52:26 +0200

    Basic Inventory

--- Assets/Scripts/MetaLogic.cs
+++ Assets/Scripts/MetaLogic.cs
@@ -7,6 +7,10 @@ public class MetaLogic : MonoBehaviour
 {
     public static bool paused=false;
 
+    public static bool inventoryIsOpen=false;
+
+    public static bool pauseMenu = false;
+
     public static void Pause()
     {
         Time.timeScale = 0;
@@ -19,5 +23,40 @@ public class MetaLogic : MonoBehaviour
         Time.timeScale = 1;
         Camera.main.GetComponent<PostProcessVolume>().enabled = false;
         paused = false;
+    }
+
+    public static void OpenPauseMenu()
+    {
+        if(inventoryIsOpen) CloseInventory();
+
+        Pause();
+        GameObject.FindGameObjectWithTag("PauseMenu").GetComponent<Canvas>().enabled = true;
+        pauseMenu = true;
+    }
+
+    public static void ClosePauseMenu()
+    {
+        Unpause();
+        GameObject.FindGameObjectWithTag("PauseMenu").GetComponent<Canvas>().enabled = false;
+        pauseMenu = false;
+    }
+
+    public static void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public static void OpenInventory()
+    {
+        Pause();
+        GameObject.FindGameObjectWithTag("Inventory").GetComponent<Canvas>().enabled = true;
+        inventoryIsOpen = true;
+    }
+
+    public static void CloseInventory()
+    {
+        Unpause();
+        GameObject.FindGameObjectWithTag("Inventory").GetComponent<Canvas>().enabled = false;
+        inventoryIsOpen = false;
     }
 }
