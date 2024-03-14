c8c7c280db92dece90f70a50e0a1756daf62b246
doximanman <kalo39570@gmail.com>
2024-03-14 17:30:47 +0200

    Basic Inventory

--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -1,15 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.EventSystems;
 
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
+    [SerializeField] private GameObject stackPrefab;
+
+    public ItemStack GetStack()
+    {
+        if (transform.childCount == 0) return null;
+        return transform.GetChild(0).GetComponent<ItemStack>();
+    }
+
+    public void SetItem(ItemStack stack)
+    {
+        stack.transform.SetParent(transform);
+    }
+
+    public void SetItem(ItemType item)
+    {
+        Assert.IsTrue(transform.childCount == 0);
+
+        GameObject newStack=Instantiate(stackPrefab,transform);
+        newStack.GetComponent<ItemStack>().Type = item;
+    }
 
     public void OnDrop(PointerEventData eventData)
     {
         // the dragged item
-        var item = eventData.pointerDrag.GetComponent<InventoryItem>();
+        var item = eventData.pointerDrag.GetComponent<ItemStack>();
 
         if(transform.childCount > 0)
         {
