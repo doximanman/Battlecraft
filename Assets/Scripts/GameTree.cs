2064aa4b795f8a110e2955658a513d21f9b2b12b
doximanman <kalo39570@gmail.com>
2024-04-24 17:10:15 +0300

    Chopping Implemented

--- Assets/Scripts/GameTree.cs
+++ Assets/Scripts/GameTree.cs
@@ -5,81 +5,35 @@ using UnityEngine.EventSystems;
 using UnityEngine.Video;
 
 
-public class GameTree : MonoBehaviour
+public class GameTree : Interactable
 {
-    enum ChoppingStatus {START,ONGOING,STOP}
-
     private float length;
     private float width;
 
-    private void Start()
+    protected override void Start()
     {
+        base.Start();
+
+        // physical length and width
         SpriteRenderer renderer = GetComponent<SpriteRenderer>();
         length = renderer.bounds.size.y;
         width = renderer.bounds.size.x;
 
-        playerControl=Player.current.GetComponent<PlayerControl>();
+        // axe to chop, chopping time proportional to length
+        requiredTool = InteractableConstants.axe;
+        chopDuration = InteractableConstants.woodChopTime * length;
     }
 
-    private ChoppingStatus status;
-    private float timer = 0;
-    public void OnMouseDown()
+    protected override void OnFinishChopping()
     {
-        timer = 0;
-        status = ChoppingStatus.START;
-    }
-
-    private PlayerControl playerControl;
-    public void OnMouseDrag()
-    {
-        // timer < 0 means the mouse wasn't clicked on this object
-        // timer >= 0 means the mouse was clicked
-        if (timer < 0) return;
-
-        // check player is close enough
-        var playerPosition=Player.current.transform.position;
-        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        if (Vector2.Distance(playerPosition,mousePosition) > InteractableConstants.maxInteractDistance) return;
-
-        // check player is holding an axe
-        if (MetaLogic.hotbar.SelectedSlot.GetStack() == null ||
-            !MetaLogic.hotbar.SelectedSlot.GetStack().Type.Equals(InteractableConstants.axe)) return;
-
-        var duration = InteractableConstants.chopTime * length;
-        // mouse was clicked and player is close enough -> start/keep counting.
-        // start animation on first time
-        timer += Time.deltaTime;
-        if (status== ChoppingStatus.START)
-        {
-            // send the direction the player should be facing
-            playerControl.Chop(duration,Mathf.Sign(mousePosition.x-playerPosition.x));
-            status = ChoppingStatus.ONGOING;
-        }
-        if (timer < duration) return;
-
         // time has passed - break the tree and add wood and sticks to inventory.
         MetaLogic.personalInventory.AddItems(InteractableConstants.wood,
             Mathf.FloorToInt(length / InteractableConstants.lengthToWood));
         MetaLogic.personalInventory.AddItems(InteractableConstants.sticks,
             Mathf.FloorToInt(width / InteractableConstants.widthToSticks));
-        
+
         // destroy the tree world object
         Destroy(gameObject);
-    }
-
-    private void OnMouseUp()
-    {
-        timer = -1;
-        if(status== ChoppingStatus.ONGOING)
-        {
-            playerControl.CancelChop();
-            status = ChoppingStatus.STOP;
-        }
-    }
-
-    private void OnMouseExit()
-    {
-        OnMouseUp();
     }
 
 }
--- Assets/Scripts/InteractableConstants.cs
+++ Assets/Scripts/InteractableConstants.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class InteractableConstants : MonoBehaviour
 {
+    public static float lengthToSticks;
     public static float lengthToWood;
     public static float widthToSticks;
-    public static float chopTime;
+    public static float woodChopTime;
+    public static float bushChopTime;
     public static ItemType wood;
     public static ItemType sticks;
     public static ItemType axe;
     public static float maxInteractDistance;
-    
 
+    [SerializeField] private float _lengthToSticks;
     [SerializeField] private float _lengthToWood;
     [SerializeField] private float _widthToSticks;
-    [SerializeField] private float _chopTime;
+    [SerializeField] private float _woodChopTime;
+    [SerializeField] private float _bushChopTime;
     [SerializeField] private ItemType _wood;
     [SerializeField] private ItemType _sticks;
     [SerializeField] private ItemType _axe;
@@ -23,12 +26,14 @@ public class InteractableConstants : MonoBehaviour
 
     private void Start()
     {
+        lengthToSticks = _lengthToSticks;
         lengthToWood = _lengthToWood;
         widthToSticks = _widthToSticks;
-        chopTime = _chopTime;
+        woodChopTime=_woodChopTime;
+        bushChopTime=_bushChopTime;
         wood = _wood;
         sticks= _sticks;
-        maxInteractDistance = _maxInteractDistance;
         axe = _axe;
+        maxInteractDistance = _maxInteractDistance;
     }
 }
