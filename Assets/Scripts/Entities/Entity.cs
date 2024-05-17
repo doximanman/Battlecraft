6da27d626b932528b97fbc4694145a0d09193e16
doximanman <kalo39570@gmail.com>
2024-05-17 22:26:03 +0300

    First Enemy!

--- Assets/Scripts/Entities/Entity.cs
+++ Assets/Scripts/Entities/Entity.cs
@@ -2,16 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Entity : IHitListener
 {
-    public string animalName;
+    public string entityName;
 
     public StackData[] droppedItems;
     public Vector2 knockback;
     public bool getsScared;
     public float runawayTime;
+    public bool chasesPlayer;
+    public float chaseRange;
+    public float chaseUntilCloserThan;
     public int maxHealth;
     private float health=1;
     public float Health
@@ -26,19 +31,44 @@ public class Entity : IHitListener
         }
     }
 
+    private EntityMovement movement;
     private void Start()
     {
         Health = maxHealth;
         Player.current.RegisterSwingListener(this);
+        movement=GetComponent<EntityMovement>();
+    }
+
+    [SerializeField] float distanceToPlayer;
+    [SerializeField] bool chasing = false;
+    private void FixedUpdate()
+    {
+        if (chasesPlayer)
+        {
+            // if this entity is of the type that can chase
+            // the player, then always evaluate distanceToPlayer.
+            distanceToPlayer = Mathf.Abs(Player.current.transform.position.x - transform.position.x);
+            if (!chasing)
+            {
+                chasing = true;
+                // follow the player position until too far away, or the
+                // entity is no longer of the type that can follow the player.
+                StartCoroutine(movement.FollowUntil(
+                    () => !(chasing = distanceToPlayer < chaseRange && chasesPlayer),
+                    () => Player.current.transform.position.x,
+                    chaseUntilCloserThan));
+            }
+        }
     }
 
     public void PlaySound(string soundName)
     {
-        AudioManager.instance.PlayFrom(gameObject,animalName, soundName);
+        AudioManager.instance.PlayFrom(gameObject,entityName, soundName);
     }
 
     public override void OnHit(ItemType hitWith)
     {
+        EntityMovement movement=GetComponent<EntityMovement>();
         // entity was hit
         if (!hitWith.swingable)
         {
@@ -51,14 +81,15 @@ public class Entity : IHitListener
             animator.SetTrigger("Hurt");
         }
         // knockback
-        if(gameObject.TryGetComponent<Rigidbody2D>(out var body))
+        if(gameObject.TryGetComponent<Rigidbody2D>(out var _))
         {
+
             bool isRight = Player.current.transform.position.x < transform.position.x;
-            if (isRight) body.velocity += knockback;
-            else body.velocity += new Vector2(-knockback.x, knockback.y);
+            movement.Launch(new Vector3(isRight ? knockback.x : -knockback.x, knockback.y,0));
+
         }
         // entity runs away
-        if(getsScared && gameObject.TryGetComponent<EntityMovement>(out var movement))
+        if(getsScared && movement!=null)
         {
             bool isRight = Player.current.transform.position.x < transform.position.x;
             StartCoroutine(movement.Run(runawayTime,isRight ? Direction.RIGHT : Direction.LEFT));
@@ -79,5 +110,79 @@ public class Entity : IHitListener
     private void KillEntity()
     {
         Destroy(gameObject);
+    }
+}
+
+/// <summary>
+/// Editor script to hide irrelavent properties
+/// (for example when "getsScared" is off, the
+/// "runawayTime" variable is irrelavent)
+/// </summary>
+[CustomEditor(typeof(Entity))]
+public class EntityEditor : Editor
+{
+    SerializedProperty entityName;
+
+    SerializedProperty droppedItems;
+    SerializedProperty knockback;
+    SerializedProperty getsScared;
+    SerializedProperty runawayTime;
+    SerializedProperty chasesPlayer;
+    SerializedProperty chaseRange;
+    SerializedProperty maxHealth;
+    SerializedProperty chasing;
+    SerializedProperty distanceToPlayer;
+    SerializedProperty chaseUntilCloserThan;
+
+    public override VisualElement CreateInspectorGUI()
+    {
+        VisualElement returnValue= base.CreateInspectorGUI();
+        entityName = serializedObject.FindProperty("entityName");
+        droppedItems = serializedObject.FindProperty("droppedItems");
+        knockback = serializedObject.FindProperty("knockback");
+        getsScared = serializedObject.FindProperty("getsScared");
+        runawayTime = serializedObject.FindProperty("runawayTime");
+        chasesPlayer = serializedObject.FindProperty("chasesPlayer");
+        chaseRange = serializedObject.FindProperty("chaseRange");
+        maxHealth = serializedObject.FindProperty("maxHealth");
+        chasing = serializedObject.FindProperty("chasing");
+        distanceToPlayer = serializedObject.FindProperty("distanceToPlayer");
+        chaseUntilCloserThan = serializedObject.FindProperty("chaseUntilCloserThan");
+        return returnValue;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUI.BeginChangeCheck();
+
+        EditorGUILayout.PropertyField(entityName);
+        EditorGUILayout.PropertyField(droppedItems);
+        EditorGUILayout.PropertyField(knockback);
+        EditorGUILayout.PropertyField(getsScared);
+        if(getsScared.boolValue)
+        {
+            EditorGUILayout.PropertyField(runawayTime);
+        }
+        EditorGUILayout.PropertyField(chasesPlayer);
+        if(chasesPlayer.boolValue)
+        {
+            EditorGUILayout.PropertyField(chaseRange);
+            EditorGUILayout.PropertyField(chaseUntilCloserThan);
+        }
+        EditorGUILayout.PropertyField(maxHealth);
+
+        if (chasesPlayer.boolValue)
+        {
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUILayout.PropertyField(distanceToPlayer);
+            EditorGUILayout.PropertyField(chasing);
+            EditorGUI.EndDisabledGroup();
+        }
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            serializedObject.ApplyModifiedProperties();
+        }
     }
 }
