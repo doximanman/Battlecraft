e56b1f3012b222150002c05c59a1c90f70e58011
doximanman <kalo39570@gmail.com>
2024-08-29 18:57:36 +0300

    More Server Implementation

--- Server/router.py
+++ Server/router.py
@@ -1,11 +1,19 @@
-async def user(request):
+import controller
+
+async def user(websocket,request):
+    subtype = request['subtype']
+    if subtype == 'login':
+        await controller.user.login(websocket,request)
+    elif subtype == 'register':
+        await controller.user.register(websocket,request)
+    else:
+        await controller.user.default(websocket,request)
+
+async def player(websocket,request):
     return
 
-async def player(request):
+async def world(websocket,request):
     return
 
-async def world(request):
-    return
-
-async def default(request):
+async def default(websocket,request):
     return
